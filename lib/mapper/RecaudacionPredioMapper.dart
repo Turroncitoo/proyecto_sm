e20da21b0f580ac3f18bc181267009b62d3fba02
Edgardo Fernandez <efernandeza@odybank.com.pe>
2023-11-18 18:25:31 -0500

    fix: añadir identificador

--- lib/mapper/RecaudacionPredioMapper.dart
+++ lib/mapper/RecaudacionPredioMapper.dart
@@ -12,23 +12,27 @@ class RecaudacionPredioMapper implements IRecaudacionPredioMapper {
       int anio, int predio) async {
     try {
       // Realizar consulta utilizando _conexion
-      final results = await _conexion.query('SELECT cas.id_casa 			AS casa,'
-          'casd.fecha_registro	AS fecha,'
-          'casd.id_deposito		  AS idDeposito,'
-          'depo.area			      AS monto,'
-          'casd.id_estado		    AS idEstado,'
-          'est.descripcion		  AS descripcionEsta'
-          'FROM public.Casa cas'
-          'INNER JOIN public.Casa_Deposito casd'
-          'ON cas.id_casa = casd.id_casa'
-          'INNER JOIN  public.Deposito depo'
-          'ON depo.id_deposito = casd.id_deposito'
-          'INNER JOIN public.Estado est'
-          'ON est.id_estado = casd.id_estado'
-          'WHERE EXTRACT(MONTH FROM casd.fecha_registro)  = @mes'
-          'AND EXTRACT(YEAR FROM casd.fecha_registro)     = @anio'
-          'AND cas.id_predio = @predio',
-          substitutionValues: {'mes': mes, 'anio': anio, 'predio':predio});
+      final results = await _conexion.query(
+        'SELECT cas.id_casa 			AS casa,'
+            '"E" + RIGHT(CONCAT("000", cas.id_predio), 3) +"-" + RIGHT(CONCAT("000", cas.id_casa), 3) AS identificador,'
+            'casd.fecha_registro	AS fecha,'
+            'casd.id_deposito		  AS idDeposito,'
+            'depo.area			      AS monto,'
+            'casd.id_estado		    AS idEstado,'
+            'est.descripcion		  AS descripcionEsta '
+            'FROM public.Casa cas '
+            'INNER JOIN public.Casa_Deposito casd '
+            'ON cas.id_casa = casd.id_casa '
+            'INNER JOIN public.Deposito depo '
+            'ON depo.id_deposito = casd.id_deposito '
+            'INNER JOIN public.Estado est '
+            'ON est.id_estado = casd.id_estado '
+            'WHERE EXTRACT(MONTH FROM casd.fecha_registro)  = @mes '
+            'AND EXTRACT(YEAR FROM casd.fecha_registro)     = @anio '
+            'AND cas.id_predio = @predio',
+        substitutionValues: {'mes': mes, 'anio': anio, 'predio': predio},
+      );
+
 
       // Convertir cada fila a un mapa
       List<Map<String, dynamic>> resultList = [];
