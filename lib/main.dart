2fe3ed57cc0326d3c6f6ddbb099d3a7a3281c00b
JL-Arroyo-18200133 <jose.arroyo5@unmsm.edu.pe>
2023-11-22 18:22:20 -0500

    Datos para main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
+/*
+     >>>>> CUS Consultar Recaudación G7 <<<<<
+
+     20200175 Fernández Altamirano Edgardo Joaquin
+     20200100 Machaca Moscaira Kevin Jesus
+     18200133 Arroyo Alania Jose Luis
+     20200225 Zacarias Vilca Brayams Jhonatan
+*/
+
 import 'package:flutter/material.dart';
 import 'package:proyecto_sm/menu_condominios.dart';
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  theme: ThemeData (brightness: Brightness.light, primaryColor:  Colors.cyan),
-  home:  MyApp(),
-));
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.light, primaryColor: Colors.cyan),
+      home: MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: MenuCondominios(),
     );
   }
 }
-
